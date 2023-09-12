b11be1965b2bfd29ae3bfca86e8642003bfef2e3
MMatiazzo <mathmatiazzo@gmail.com>
2023-09-12 18:55:16 -0300

    feat: transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,22 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 import logoDtMoney from '../../assets/logo-dt-money.svg';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export function Header() {
 	return (
 		<HeaderContainer>
 			<HeaderContent>
 				<img src={logoDtMoney} alt="" />
-				<NewTransactionButton>Nova Transação</NewTransactionButton>
+
+				<Dialog.Root>
+					<Dialog.Trigger asChild>
+						<NewTransactionButton>Nova Transação</NewTransactionButton>
+					</Dialog.Trigger>
+
+					<NewTransactionModal></NewTransactionModal>
+				</Dialog.Root>
 			</HeaderContent>
 		</HeaderContainer>
 	);
